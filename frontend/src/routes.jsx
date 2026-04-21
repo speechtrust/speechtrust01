83a78aaea0012d922ecf3a1e63129bcc1d0264f6
Aksh Bhatnagar <akshbhatnagar2111@gmail.com>
2026-04-21 23:06:35 +0530

    v1

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,11 +1,11 @@
-import { createBrowserRouter } from "react-router-dom"
-import HomePage from "./components/home/HomePage";
-import Dashboard from "./components/dashboard/Hero";
-import Interview from "./components/assessment/Interview";
-import Result from "./components/assessment/result";
-import LoginPage from "./components/auth/LoginPage";
-import SignupPage from "./components/auth/SignupPage";
-import ProtectedRoute from "./components/commons/ProtectedRoute";
+import { createBrowserRouter } from "react-router-dom.jsx"
+import HomePage from "./components/home/HomePage.jsx";
+import Dashboard from "./components/dashboard/Hero.jsx";
+import Interview from "./components/assessment/Interview.jsx";
+import Result from "./components/assessment/Result.jsx";
+import LoginPage from "./components/auth/LoginPage.jsx";
+import SignupPage from "./components/auth/SignupPage.jsx";
+import ProtectedRoute from "./components/commons/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
     {
