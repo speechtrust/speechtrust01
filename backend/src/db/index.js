1deea614dc899223128e42aee447a734b29c3fee
Ashishmeena1210 <ashish.personal1210@gmail.com>
2026-05-23 20:00:17 +0530

    dns resolve

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
 import { DB_NAME } from '../constants.js';
+import dns from 'dns';
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
     try {
